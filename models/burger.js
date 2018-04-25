55cef5b60d94bd58b3250db4aceab2b9f144317f
Jason M. Aron <jasonaronart@gmail.com>
2018-04-25 16:50:30 -0400

    Bug Fix

--- models/burger.js
+++ models/burger.js
@@ -4,27 +4,29 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-  all: function(cb) {
-    orm.all("burgers", function(res) {
+  all: function (cb) {
+    orm.all("burgers", function (res) {
       cb(res);
     });
   },
 
-  create: function(cols, vals, cb) {
-    orm.create("burgers", cols, vals. function(res) {
+  create: function (cols, vals, cb) {
+    orm.create("burgers", cols, vals, function (res) {
       cb(res);
     });
   },
-  update: function(condition, cb) {
-    orm.delete("burgers", objColVals, condition, function(res) {
+
+  update: function (condition, cb) {
+    orm.delete("burgers", objColVals, condition, function (res) {
       cb(res);
     });
   },
-  delete: function(condition, cb) {
-    orm.delete("cats", condition, function(res) {
+
+  delete: function (condition, cb) {
+    orm.delete("burgers", condition, function (res) {
       cb(res);
     });
   }
 };
 
-module.exports = cat;
+module.exports = burger;
